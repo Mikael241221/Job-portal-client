ff64cad26cbcc221199c0052f9fa596d8ca62aed
Mikael241221 <bashawdejenu1221@gmail.com>
2025-03-23 14:57:03 -0700

    updating the banner file

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Banner from '../components/Banner'
 
 const Home = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetch("jobs.json").then(res => res.json()).then(data => {
+      console.log(data)
+    })
+  },[])
+   const [query, setQuery] =useState("");
+    const handleInputChange = (event) => {
+     setQuery(event.target.value)
+     
+    }
+    
   return (
     <div> 
-      <Banner/>
+      <Banner query ={query} handleInputChange={handleInputChange}/>
     </div>
   )
 }
