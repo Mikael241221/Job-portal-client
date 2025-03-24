26ec8d5deb06dda42471d709a8353a1e6aff655a
Mikael241221 <bashawdejenu1221@gmail.com>
2025-03-24 08:46:35 -0700

    create two files

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Banner from '../components/Banner'
+import Card from '../components/Card';
+import Jobs from './Jobs';
 
 const Home = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -20,11 +22,48 @@ const Home = () => {
     }
     // filter jobs by title
     const filteredItems = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1);
-    console.log(filteredItems)
+    // .........Radio filtering ..........
+    const handleChange = (event) => {
+      selectedCategory(event.target.value)
+    }
     
+     // .........Button based filtering ..........
+    const hanleClick = (event) => {
+      selectedCategory(event.tartget.value)
+    }
+    // main function 
+    const filteredData = (jobs, selected, query) => {
+      let filteredjobs =jobs;
+      // filtering input items
+      if (query){
+        filteredjobs = filteredItems
+      }
+      // category filtering
+      if (selected){
+        filteredjobs = filteredjobs.filter(({
+        maxPrice,
+        salaryType,
+        jobLocation,
+        postingDate,
+        experienceLevel,
+        employmentType,
+        }) =>(
+          jobLocation.toLowerCase() === selected.toLowerCase() ||
+          parseInt(maxPrice) <= parseInt(selected ) ||
+          salaryType.toLowerCase() === selected.toLowerCase() ||
+          employmentType.toLowerCase() === selected.toLowerCase()
+        ));
+        console.log(filteredjobs)
+      }
+      return filteredjobs.map((data,i) => <Card key={i} data={data}/>)
+    }
+    const result = filteredData(jobs, selectedCategory,query);
   return (
     <div> 
       <Banner query ={query} handleInputChange={handleInputChange}/>
+      <div>
+        <Jobs result ={result}/>
+      </div>
     </div>
   )
 }
